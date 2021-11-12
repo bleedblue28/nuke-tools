c34c27903e109f9de9813a6311ae5a970528fbd8
bleedblue28 <94143371+bleedblue28@users.noreply.github.com>
2021-11-12 15:13:31 -0600

    Add files via upload
    Add init.py file

---
+++ init.py
@@ -0,0 +1,16 @@
+#
+#
+#  Copyright (c) 2014, 2015, 2016, 2017 Psyop Media Company, LLC
+#  See license.txt
+#
+#
+
+import cryptomatte_utilities
+cryptomatte_utilities.setup_cryptomatte()
+
+## init.py
+## loaded by nuke before menu.py
+
+nuke.pluginAddPath('./AP_gizmos')
+
+nuke.pluginAddPath("C:\Users\Connor Adams\.nuke\NukeSurvivalToolkit")
